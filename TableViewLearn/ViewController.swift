0bdeffb933f5492e34ea3c4a17d99a786d467588
渔夫 <vzheng56@gmail.com>
2015-02-09 16:03:33 +0800

    Initial Commit

---
+++ TableViewLearn/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TableViewLearn
+//
+//  Created by mini4s220 on 15/2/9.
+//  Copyright (c) 2015年 EnjoyThis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+

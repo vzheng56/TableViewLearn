239c663b25843cf20d13e479333a86c6172baf25
渔夫 <vzheng56@gmail.com>
2015-02-09 16:42:59 +0800

    show TableView
    Can show tableView

--- TableViewLearn/ViewController.swift
+++ TableViewLearn/ViewController.swift
@@ -8,16 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
 
+  
+  @IBOutlet weak var MainTableView: UITableView!
   override func viewDidLoad() {
     super.viewDidLoad()
+    
     // Do any additional setup after loading the view, typically from a nib.
+    MainTableView.delegate = self
+    MainTableView.dataSource = self
   }
 
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
+  }
+  
+  func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    //
+    
+    return 10;
+  }
+  
+  func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    //
+    let cell = self.MainTableView.dequeueReusableCellWithIdentifier("maintableviewcell") as UITableViewCell
+    
+    var label01 = cell.viewWithTag(100) as UILabel
+    var label02 = cell.viewWithTag(101) as UILabel
+    
+    label01.text = "Hi"
+    label02.text = "2013-12-1"
+    
+    return cell
   }
 
 
